e76c1f67c6d902de5e5c5cbaa8f4b277446c488e
Stefan <stefanacimovic1117@gmail.com>
2023-03-09 21:18:34 +0100

    Fixed social icons.

--- lib/main.dart
+++ lib/main.dart
@@ -62,43 +62,72 @@ class MyApp extends StatelessWidget {
                     child: HomePage(),
                   ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const SizedBox(width: 15),
-                      IconButton(
-                        tooltip:
-                            'Found a bug or you have a suggestion?\nPlease create issue in the GitHub repo and try it to explain as much as you can.\nThank you!',
-                        splashColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        icon: const FaIcon(
-                          FontAwesomeIcons.github,
-                          color: ThemeConstants.fourthColor,
-                        ),
-                        onPressed: () async {
-                          const gitHubUrl =
-                              'https://github.com/Hazaaa/YouTube-To-Mp3-v2';
+                      Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          IconButton(
+                            tooltip:
+                                'Found a bug or you have a suggestion?\nPlease create issue in the GitHub repo and try it to explain as much as you can.\nThank you!',
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            icon: const FaIcon(
+                              FontAwesomeIcons.bugs,
+                              color: ThemeConstants.fourthColor,
+                            ),
+                            onPressed: () async {
+                              const gitHubIssuesNewUrl =
+                                  'https://github.com/Hazaaa/YouTube-To-Mp3-v2/issues/new';
 
-                          if (await canLaunchUrlString(gitHubUrl)) {
-                            await launchUrlString(gitHubUrl);
-                          }
-                        },
+                              if (await canLaunchUrlString(
+                                  gitHubIssuesNewUrl)) {
+                                await launchUrlString(gitHubIssuesNewUrl);
+                              }
+                            },
+                          ),
+                        ],
                       ),
-                      const SizedBox(width: 10),
-                      IconButton(
-                        tooltip: 'You can follow me on Medium :)',
-                        splashColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        icon: const FaIcon(
-                          FontAwesomeIcons.medium,
-                          color: ThemeConstants.fourthColor,
-                        ),
-                        onPressed: () async {
-                          const mediumUrl =
-                              'https://medium.com/@stefanacimovicMEng';
+                      Row(
+                        children: [
+                          IconButton(
+                            tooltip:
+                                'If you like the project give it a star and follow it on GitHub :)',
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            icon: const FaIcon(
+                              FontAwesomeIcons.github,
+                              color: ThemeConstants.fourthColor,
+                            ),
+                            onPressed: () async {
+                              const gitHubUrl =
+                                  'https://github.com/Hazaaa/YouTube-To-Mp3-v2';
 
-                          if (await canLaunchUrlString(mediumUrl)) {
-                            await launchUrlString(mediumUrl);
-                          }
-                        },
+                              if (await canLaunchUrlString(gitHubUrl)) {
+                                await launchUrlString(gitHubUrl);
+                              }
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          IconButton(
+                            tooltip: 'You can follow me on Medium :)',
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            icon: const FaIcon(
+                              FontAwesomeIcons.medium,
+                              color: ThemeConstants.fourthColor,
+                            ),
+                            onPressed: () async {
+                              const mediumUrl =
+                                  'https://medium.com/@stefanacimovicMEng';
+
+                              if (await canLaunchUrlString(mediumUrl)) {
+                                await launchUrlString(mediumUrl);
+                              }
+                            },
+                          ),
+                          const SizedBox(width: 15)
+                        ],
                       )
                     ],
                   )
